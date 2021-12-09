57144f122e55ee1610779b307d3935918b28167e
xjd <xjd@cryptape.com>
2021-12-09 17:41:25 +0800

    replace more instructions

--- src/main.rs
+++ src/main.rs
@@ -146,15 +146,19 @@ fn remove_a_instruction(data: &mut Vec<u8>, pc: u64) {
         len = 4;
     }
     let pc2 = pc as usize;
-    if len == 4 && &data[pc2..pc2 + 4] == &[0xaf, 0xa7, 0xe6, 0x04] {
+    if len == 4
+        && (
+            // amoadd.w.aq	a5,a4,(a3)
+            &data[pc2..pc2 + 4] == &[0xaf, 0xa7, 0xe6, 0x04] ||
+            // amoswap.d.aq	a4,a4,(a5)
+            &data[pc2..pc2+4] == &[0x2f, 0xb7,  0xe7, 0x0c]
+        )
+    {
         data[pc2] = 0x13; // nop
         data[pc2 + 1] = 0;
         data[pc2 + 2] = 0;
         data[pc2 + 3] = 0;
-        println!(
-            "instruction at {}(content 04e6a7af) is replace by nop",
-            pc2
-        );
+        println!("instruction at {}(content 04e6a7af) is replace by nop", pc2);
     }
 }
 
